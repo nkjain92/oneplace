77ba68016fdbf19c7a59163f3750ae898e91320d
Nishank Jain <nk.jain92@live.com>
2025-03-10 13:47:04 +0530

    .env updated from next_public to just supabase url and supabase anon

--- middleware.ts
+++ middleware.ts
@@ -7,23 +7,19 @@ const protectedRoutes = ['/subscriptions'];
 const authRoutes = ['/login', '/signup'];
 
 export async function middleware(req: NextRequest) {
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return req.cookies.get(name)?.value;
-        },
-        set() {
-          // No-op: Middleware doesn't set cookies
-        },
-        remove() {
-          // No-op: Middleware doesn't remove cookies
-        },
+  const supabase = createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
+    cookies: {
+      get(name: string) {
+        return req.cookies.get(name)?.value;
+      },
+      set() {
+        // No-op: Middleware doesn't set cookies
+      },
+      remove() {
+        // No-op: Middleware doesn't remove cookies
       },
     },
-  );
+  });
 
   const {
     data: { session },
